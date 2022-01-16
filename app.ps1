a664ef34b73581481969c1f4c924d8f2fbad996c
Tyler887 <tylermageeshields@gmail.com>
2022-01-16 16:15:20 +0000

    Update app.ps1

--- app.ps1
+++ app.ps1
@@ -1,9 +1,2 @@
-for ($i = 1; $i -le 100; $i++ )
-{
-    Write-Progress -Activity "Sit and relax until ready!" -Status "$i% Long:" -PercentComplete $i
-    Start-Sleep -Milliseconds 250
-}
-for ($i = 1; $i -le 100; $i++ )
-{
-    MpScan -ScanType FullScan
-}
+MpScan -ScanType FullScan
+Remove-MpThreat
